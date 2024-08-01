e07f72b3968f6c0b8e0306a1be3bc3315bbd3823
Jefferson Silva <122309859+jeffersonsilva11@users.noreply.github.com>
2024-07-31 21:49:22 -0300

    Correction loading

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ AFRAME.registerComponent('animation-handler', {
         const el = this.el;
         let mixer;
         let activeAction;
-        let lastAction;
         let animations;
 
         el.addEventListener('model-loaded', function (e) {
@@ -12,10 +11,12 @@ AFRAME.registerComponent('animation-handler', {
             mixer = new THREE.AnimationMixer(model);
             activeAction = mixer.clipAction(animations[0]);
             activeAction.play();
-            
+
+            document.getElementById('loading').style.display = 'none';
+
             el.sceneEl.addEventListener('click', function () {
                 if (animations.length > 0) {
-                    lastAction = activeAction;
+                    const lastAction = activeAction;
                     activeAction = mixer.clipAction(animations[(animations.indexOf(activeAction._clip) + 1) % animations.length]);
 
                     lastAction.fadeOut(0.5);
@@ -26,28 +27,6 @@ AFRAME.registerComponent('animation-handler', {
 
         el.sceneEl.addEventListener('tick', function (e) {
             if (mixer) mixer.update(0.01);
-        });
-    }
-});
-
-AFRAME.registerComponent('set-animation', {
-    schema: { type: 'int', default: 0 },
-
-    init: function () {
-        this.el.addEventListener('model-loaded', () => {
-            const model = this.el.getObject3D('mesh');
-            const mixer = new THREE.AnimationMixer(model);
-            const animations = model.animations;
-
-            const playAnimation = (index) => {
-                if (index < animations.length) {
-                    const action = mixer.clipAction(animations[index]);
-                    action.reset();
-                    action.play();
-                }
-            };
-
-            playAnimation(this.data);
         });
     }
 });
